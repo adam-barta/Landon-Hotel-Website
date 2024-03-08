58818ecded9c2a8587882728de690b6d33085b92
Adam <64394877+lparsonsp@users.noreply.github.com>
2024-03-07 19:26:15 -0800

    B1. Modify for localization B.  Modify the Landon Hotel scheduling application for localization and internationalization by doing the following:
    1.   Install the Landon Hotel scheduling application in your integrated development environment (IDE). Modify the Java classes of application to display a welcome message by doing the following:
    
    a.  Build resource bundles for both English and French (languages required by Canadian law). Include a welcome message in the language resource bundles.
    
    b.  Display the welcome message in both English and French by applying the resource bundles using a different thread for each language.

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,27 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.controller.WelcomeMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+
+		WelcomeMessage welcomeMessageEnglish = new WelcomeMessage(Locale.US);
+		WelcomeMessage welcomeMessageFrench = new WelcomeMessage(Locale.CANADA_FRENCH);
+
+		Thread englishWelcomeThread = new Thread(welcomeMessageEnglish);
+		Thread frenchWelcomeThread = new Thread(welcomeMessageFrench);
+
+		englishWelcomeThread.start();
+		frenchWelcomeThread.start();
 	}
 
 }
